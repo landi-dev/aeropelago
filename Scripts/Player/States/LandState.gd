7e3dad2fea9e446ae10003869ee484968dac6d5c
Sociopathix <sociopathix.221b@gmail.com>
2023-07-15 15:16:30 -0500

    Transfer to 4.0: LandState.gd -> physics_process( ) - Copied over the physics_process( ) function to LandState.gd. - Player code is still dysfunctional.

--- Scripts/Player/States/LandState.gd
+++ Scripts/Player/States/LandState.gd
@@ -17,3 +17,47 @@ func input(event : InputEvent) -> int:
 	player.move_direction = player.move_direction.rotated(Vector3.UP, player.spring_arm.rotation.y).normalized();
 	
 	return STATE.NULL;
+
+func physics_process(delta : float) -> int:
+	
+	# Add a small amount of gravity to clamp the player down.
+	if player.is_on_floor():
+		
+		player.y_velocity = -0.01;
+	
+	elif player.floor_raycast.is_colliding():
+		
+		player.y_velocity = clamp(player.y_velocity - player.gravity, -player.terminal_velocity, player.terminal_velocity);
+	
+	else:
+		
+		return STATE.FALL;
+	
+	# If the character is not currently moving in a given direction, slow them down by friction.
+	if player.move_direction.x == 0:
+		
+		player.velocity.x = lerp(player.velocity.x, 0, player.friction * delta);
+	
+	if player.move_direction.z == 0:
+		
+		player.velocity.z = lerp(player.velocity.z, 0, player.friction * delta);
+	
+	# If the player isn't moving, change to IDLE state.
+	if abs(player.velocity.x) == 0 and abs(player.velocity.z) == 0 and !player.animation_player.is_playing():
+		
+		return STATE.IDLE;
+	
+	elif !player.animation_player.is_playing():
+		
+		return STATE.SPRINT;
+	
+	# Finally, adjust the y-velocity after x and z calculations have been made.
+	player.velocity.y = player.y_velocity;
+	player.move_and_slide();
+	
+	# If the player is fallling, change to FALL state.
+	if !player.is_on_floor():
+		
+		return STATE.FALL
+	
+	return STATE.NULL
